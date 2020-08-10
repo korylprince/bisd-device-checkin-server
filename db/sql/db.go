bb3188557c3970de1327a67a494fabe7500ecc8b
Kory Prince <korylprince@gmail.com>
2020-08-10 12:30:36 -0500

    remove device-specific SQL

--- db/sql/db.go
+++ db/sql/db.go
@@ -51,7 +51,7 @@ func (d *DB) CreateCharge(tx *sql.Tx, charges db.Charges, inventoryNumber, user,
 func (d *DB) ReadDevice(tx *sql.Tx, bagTag string) (*db.Device, error) {
 	device := &db.Device{BagTag: bagTag}
 
-	row := tx.QueryRow("SELECT id, inventory_number, serial_number, status, model, user, notes FROM devices WHERE bag_tag=? AND (model = 'C732T-C8VY' OR model = 'Chromebook 3100');", bagTag)
+	row := tx.QueryRow("SELECT id, inventory_number, serial_number, status, model, user, notes FROM devices WHERE bag_tag=?;", bagTag)
 
 	err := row.Scan(
 		&(device.ID),
